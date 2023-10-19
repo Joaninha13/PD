f690367057646a491dd41910d06647764485065c
Joaninha13 <66388508+Joaninha13@users.noreply.github.com>
2023-10-19 22:01:07 +0100

    Testes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("sdfdhgfgfsa");
+        System.out.println("sdfdhgfgfsa");
+        System.out.println("sdfdhgfgfsa");
+        System.out.println("sdfdhgfgfsa");
         System.out.println("Hello world!");
+
+
     }
 }

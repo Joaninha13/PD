e326a30b6c7f6705277bdbde3fd983bc9f6f2564
mltavares <miguel-tavares10@hotmail.com>
2023-10-30 18:04:59 +0000

    nova ClienteApp e ClientCommunication só para testar a ligação ao servidor

--- src/Clientes/communication/ClientCommunication.java
+++ src/Clientes/communication/ClientCommunication.java
@@ -1,5 +1,46 @@
 package Clientes.communication;
 
+import share.login.login;
+import share.registo.registo;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ClientCommunication {
 
+    private InetAddress serverAddr;
+    private int serverPort;
+
+    public ClientCommunication(String serverAddress, int serverPort) throws UnknownHostException {
+        this.serverAddr = InetAddress.getByName(serverAddress);
+        this.serverPort = serverPort;
+    }
+
+    public login authenticateUser(login userLogin) throws IOException, ClassNotFoundException {
+        return sendObject(userLogin);
+    }
+
+    public registo registerUser(registo userRegister) throws IOException, ClassNotFoundException {
+        return sendObject(userRegister);
+    }
+
+    public String sendMessage(String message) throws IOException, ClassNotFoundException {
+        return sendObject(message);
+    }
+
+    private <T> T sendObject(Object obj) throws IOException, ClassNotFoundException {
+        try (Socket socket = new Socket(serverAddr, serverPort);
+             ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream oin = new ObjectInputStream(socket.getInputStream())) {
+
+            oout.writeObject(obj);
+            oout.flush();
+
+            return (T) oin.readObject();
+        }
+    }
 }
--- src/Clientes/main/ClienteApp.java
+++ src/Clientes/main/ClienteApp.java
@@ -1,65 +1,23 @@
 package Clientes.main;
 
-import java.util.Scanner;
+import Clientes.communication.ClientCommunication;
+import share.login.login;
 
 public class ClienteApp {
-
     public static void main(String[] args) {
+        try {
+            ClientCommunication communication = new ClientCommunication("localhost", 1234);
 
-        Scanner sc = new Scanner(System.in);
-        int escolha;
+            // Enviar uma String
+            communication.sendMessage("Hello Server!");
 
-        do {
+            // Enviar loginb
+            login userLogin = new login("example@email.com", "password123");
+            communication.authenticateUser(userLogin);
 
-            System.out.println("Escolha uma opção:");
-            System.out.println("1. Criar Evento");
-            System.out.println("2. Editar Evento.");
-            System.out.println("3. Eliminar Evento.");
-            System.out.println("4. Consultar Evento Criados.");
-            System.out.println("5. Gera Codigo Evento.");
-            System.out.println("6. Consulta Presenças."); // Obtenção de um ficheiro csv com a lista de presenças
-            System.out.println("7. Consulta dos eventos de um utilizador."); // Obtenção de um ficheiro csv com a lista de presenças
-            System.out.println("8. Eliminar presença registada.");
-            System.out.println("9. Inserção de presença manual.");
-            System.out.println("10. Logout");
-
-            escolha = sc.nextInt();
-
-            switch (escolha) {
-                case 1:
-
-                    break;
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-
-                    break;
-                case 5:
-
-                    break;
-                case 6:
-
-                    break;
-                case 7:
-
-                    break;
-                case 8:
-
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    System.out.println("Saindo do programa.");
-                    break;
-                default:
-                    System.out.println("Opção inválida. Escolha novamente.");
-            }
-
-        }while (escolha != 10);
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
-
 }
+

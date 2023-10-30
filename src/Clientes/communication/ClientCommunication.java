ccceb4627d07cd610532856c5b80bd3f79eaa225
mltavares <miguel-tavares10@hotmail.com>
2023-10-30 22:28:33 +0000

    ClienteApp e ClientCommunication (ainda não completos)

--- src/Clientes/communication/ClientCommunication.java
+++ src/Clientes/communication/ClientCommunication.java
@@ -1,14 +1,13 @@
 package Clientes.communication;
 
-import share.login.login;
-import share.registo.registo;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import share.login.login;
+import share.registo.registo;
 
 public class ClientCommunication {
 
@@ -21,26 +20,28 @@ public class ClientCommunication {
     }
 
     public login authenticateUser(login userLogin) throws IOException, ClassNotFoundException {
-        return sendObject(userLogin);
-    }
-
-    public registo registerUser(registo userRegister) throws IOException, ClassNotFoundException {
-        return sendObject(userRegister);
-    }
-
-    public String sendMessage(String message) throws IOException, ClassNotFoundException {
-        return sendObject(message);
-    }
-
-    private <T> T sendObject(Object obj) throws IOException, ClassNotFoundException {
         try (Socket socket = new Socket(serverAddr, serverPort);
              ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
              ObjectInputStream oin = new ObjectInputStream(socket.getInputStream())) {
 
-            oout.writeObject(obj);
+            oout.writeObject(userLogin);
             oout.flush();
 
-            return (T) oin.readObject();
+            return (login) oin.readObject();
         }
     }
+
+    public registo registerUser(registo userRegister) throws IOException, ClassNotFoundException {
+        try (Socket socket = new Socket(serverAddr, serverPort);
+             ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream oin = new ObjectInputStream(socket.getInputStream())) {
+
+            oout.writeObject(userRegister);
+            oout.flush();
+
+            return (registo) oin.readObject();
+        }
+    }
+
+    // Falta adicionar o resto das funcionalidades
 }
--- src/Clientes/main/ClienteApp.java
+++ src/Clientes/main/ClienteApp.java
@@ -1,23 +1,79 @@
 package Clientes.main;
 
+import java.util.Scanner;
 import Clientes.communication.ClientCommunication;
 import share.login.login;
+import share.registo.registo;
 
 public class ClienteApp {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ClientCommunication communication;
+
         try {
-            ClientCommunication communication = new ClientCommunication("localhost", 1234);
+            communication = new ClientCommunication("serverAddress", 1234); // Substitua com o endereço e porta corretos
 
-            // Enviar uma String
-            communication.sendMessage("Hello Server!");
+            int escolha;
+            do {
+                System.out.println("Escolha uma opção:");
+                System.out.println("1. Registrar");
+                System.out.println("2. Fazer Login");
+                System.out.println("3. Sair");
 
-            // Enviar loginb
-            login userLogin = new login("example@email.com", "password123");
-            communication.authenticateUser(userLogin);
+                escolha = sc.nextInt();
+                sc.nextLine(); // Limpar o buffer
+
+                switch (escolha) {
+                    case 1:
+                        System.out.println("Introduza o seu nome");
+                        String nome = sc.nextLine();
+                        System.out.println("Introduza o seu numero de identificação");
+                        String numID = sc.nextLine();
+                        System.out.println("Introduza o seu email:");
+                        String email = sc.nextLine();
+                        System.out.println("Introduza a sua password:");
+                        String password = sc.nextLine();
+
+                        registo userRegister = new registo(nome, numID, email, password);
+                        registo responseRegister = communication.registerUser(userRegister);
+
+                        if(responseRegister.isRegistered()) {
+                            System.out.println(responseRegister.getMsg());
+                        } else {
+                            System.out.println("Erro no registo");
+                        }
+                        break;
+
+                    case 2:
+                        System.out.println("Introduza o seu email:");
+                        email = sc.nextLine();
+                        System.out.println("Introduza a sua password:");
+                        password = sc.nextLine();
+
+                        login userLogin = new login(email, password);
+                        login responseLogin = communication.authenticateUser(userLogin);
+
+                        if(responseLogin.isValid()) {
+                            System.out.println("Autenticação bem-sucedida!");
+                        } else {
+                            System.out.println("Erro de autenticação: " + responseLogin.getMsg());
+                        }
+                        break;
+
+                    case 3:
+                        System.out.println("A sair do programa...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Escolha novamente.");
+                }
+
+            } while (escolha != 3);
 
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
+        } finally {
+            sc.close();
         }
     }
 }
-

757b30b5027b6ac2c0c7f948a3b15836b25f7990
Daniel Rodrigues <107119895+daniel2dd1@users.noreply.github.com>
2023-11-18 21:36:01 +0000

    Update servidorBackup.java
    ponto 2 do sv backup

--- src/Servidores/servidorBackup.java
+++ src/Servidores/servidorBackup.java
@@ -1,10 +1,27 @@
 package Servidores;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class servidorBackup {
 
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        boolean isEmptyDirectory;
+
+        if(args.length != 1){
+            System.out.println("Sintaxe: java servidorBackup <dir_replica_bd>");
+            return;
+        }
+
+        Path dirRepBd = Paths.get(args[1]);
+
+        //verifica se a diretoria não está vazia
+        if((isEmptyDirectory = Files.list(dirRepBd).findAny().isPresent()) == false){
+            System.out.println("Diretoria não está vazia!\n");
+            return;
+        }
     }
 }

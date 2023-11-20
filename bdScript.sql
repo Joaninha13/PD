90015ef18429be00c3c87977c0c4eddd1759fa58
Joaninha13 <66388508+Joaninha13@users.noreply.github.com>
2023-11-20 12:20:42 +0000

    bdScript.sql changed a little

--- bdScript.sql
+++ bdScript.sql
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS Codigo_Registo (codigo Varchar(100),Tempo INTEGER,Eve
 CREATE TABLE IF NOT EXISTS Utilizadores (Numero_Indentificacao INTEGER PRIMARY KEY, Nome Varchar(100), Email Varchar(100), Password Varchar(100));
 
 -- Inserir um utilizador na tabela Utilizadores
-INSERT INTO Utilizadores (Numero_Indentificacao, Nome, Email, Password) VALUES ('1', 'admin', 'admin@isec.com', 'admin');
+INSERT INTO Utilizadores (Numero_Indentificacao, Nome, Email, Password) VALUES ('1', 'admin', 'admin@isec.pt', 'admin');
 
 CREATE TABLE IF NOT EXISTS Presencas (Evento_Designacao Varchar(100),Utilizador_ID INTEGER,FOREIGN KEY (Evento_Designacao) REFERENCES Evento (Designacao), FOREIGN KEY (Utilizador_ID) REFERENCES Utilizadores (Numero_Indentificacao));
 
 -- para teste
 
---INSERT INTO EVENTO (Designacao, Localidade, Data, Hora_Inicio, Hora_Fim) VALUES ('Evento1', 'Leiria', '2020-12-12', '12:00:00', '13:00:00');
+--INSERT INTO EVENTO (Designacao, Localidade, Data, Hora_Inicio, Hora_Fim) VALUES ('Evento1', 'Leiria', '1, '12:00:00', '13:00:00');
 --INSERT INTO EVENTO (Designacao, Localidade, Data, Hora_Inicio, Hora_Fim) VALUES ('Evento2', 'COIMBRA', '2020-12-12', '12:00:00', '13:00:00');
 --INSERT INTO EVENTO (Designacao, Localidade, Data, Hora_Inicio, Hora_Fim) VALUES ('Evento3', 'Lisboa', '2020-12-12', '12:00:00', '13:00:00');
 
